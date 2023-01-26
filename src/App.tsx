b41f727af7a5844df280f657e989df6c837fe33a
Artur Nagaycev <artur.nagaycev@gmail.com>
2023-01-26 16:04:33 +0200

    pushed the footer to the bottom when the content is less than the screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Content } from './components/Content';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import styles from './App.module.scss';
 
 export const App: React.FC = () => {
   return (
-    <div>
+    <div className={styles.page}>
       <Header />
       <Content />
       <Footer />
