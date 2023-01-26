7dbe2e3aeec69547fc8df00df2b20424c3353f25
Artur Nagaycev <artur.nagaycev@gmail.com>
2023-01-26 14:44:33 +0200

    added loader component while loading

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { getProducts } from '../../api/products';
 import { Product } from '../../types/Product';
+import { Loader } from '../Loader';
 import { ProductCard } from '../ProductCard';
 import styles from './ProductList.module.scss';
 
 export const ProductList: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getProductsFromServer = async () => {
     try {
+      setIsLoading(true);
       const productsFromServer = await getProducts();
 
       setProducts(productsFromServer);
     } catch (error: any) {
       throw new Error(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -23,21 +28,26 @@ export const ProductList: React.FC = () => {
 
   return (
     <section className={styles.products}>
-      <h2 className={styles.products__title}>Product Page</h2>
-      <ul className={styles.products__content}>
-        {products.map((product) => {
-          const { id, image, breed_group, name } = product;
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <>
+          <h2 className={styles.products__title}>Product Page</h2>
+          <ul className={styles.products__content}>
+            {products.map((product) => {
+              const { id, image, breed_group, name } = product;
 
-          return (
-            <ProductCard
-              key={id}
-              imageUrl={image.url}
-              category={breed_group || 'No category'}
-              title={name}
-            />
-          );
-        })}
-      </ul>
+              return (
+                <ProductCard
+                  key={id}
+                  imageUrl={image.url}
+                  category={breed_group || 'No category'}
+                  title={name}
+                />
+              );
+            })}
+          </ul>
+        </>
+      )}
     </section>
   );
 };
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -5,17 +5,22 @@ import { Product } from '../../types/Product';
 import { ProductCard } from '../ProductCard';
 import styles from './Slider.module.scss';
 import arrowImg from '../../assets/img/arrow_right.svg';
+import { Loader } from '../Loader';
 
 export const Slider: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getProductsFromServer = async () => {
     try {
+      setIsLoading(true);
       const productsFromServer = await getProducts();
 
       setProducts(productsFromServer);
     } catch (error: any) {
       throw new Error(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -26,44 +31,48 @@ export const Slider: React.FC = () => {
 
   return (
     <section className={styles.slider}>
-      <h3 className={styles.slider__title}>Home Page</h3>
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <>
+          <h3 className={styles.slider__title}>Home Page</h3>
+          <div className={styles.slider__content}>
+            <ul className={styles.slider__items}>
+              {products.slice(0, 8).map((product) => {
+                const { id, image, breed_group, name } = product;
 
-      <div className={styles.slider__content}>
-        <ul className={styles.slider__items}>
-          {products.slice(0, 8).map((product) => {
-            const { id, image, breed_group, name } = product;
+                return (
+                  <li className={styles.slider__item} key={id}>
+                    <ProductCard
+                      imageUrl={image.url}
+                      category={breed_group || 'No category'}
+                      title={name}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
 
-            return (
-              <li className={styles.slider__item} key={id}>
-                <ProductCard
-                  imageUrl={image.url}
-                  category={breed_group || 'No category'}
-                  title={name}
-                />
-              </li>
-            );
-          })}
-        </ul>
-
-        <button
-          className={classNames(
-            styles.slider__button,
-            styles['slider__button--left']
-          )}
-          type="button"
-        >
-          <img src={arrowImg} alt="arrow" />
-        </button>
-        <button
-          className={classNames(
-            styles.slider__button,
-            styles['slider__button--right']
-          )}
-          type="button"
-        >
-          <img src={arrowImg} alt="arrow" />
-        </button>
-      </div>
+            <button
+              className={classNames(
+                styles.slider__button,
+                styles['slider__button--left']
+              )}
+              type="button"
+            >
+              <img src={arrowImg} alt="arrow" />
+            </button>
+            <button
+              className={classNames(
+                styles.slider__button,
+                styles['slider__button--right']
+              )}
+              type="button"
+            >
+              <img src={arrowImg} alt="arrow" />
+            </button>
+          </div>
+        </>
+      )}
     </section>
   );
 };
